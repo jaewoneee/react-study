1592375c8f985add75fb7f3ad7731718abd71bd8
jaewoneee <jaewon810@naver.com>
2021-12-13 11:15:49 +0900

    scss 문법 활용

--- shop/src/detail.js
+++ shop/src/detail.js
@@ -1,10 +1,8 @@
 // react 컴포넌트 만들때는 꼭 import React 해야함!
 import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import styled from 'styled-components';
+import './Detail.scss';
 
-let box = styled.div`padding:20px;`;
-let name = styled.h4`font-size:20px;font-weight:bold`;
 
 
 function Detail(props) {
@@ -16,9 +14,15 @@ function Detail(props) {
     
     return (
         <div className="container">
-            <box>
-                <name>뭐야</name>
-             </box>
+            <div className='my-alert'>
+                <p>재고가 얼마 남지 않았습니다</p>
+            </div>
+             <div className='my-alert2'>
+                <p>재고가 얼마 남지 않았습니다!</p>
+            </div>
+             <div className='my-alert3'>
+                <p>듸용</p>
+            </div>
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -29,9 +33,9 @@ function Detail(props) {
                     <p>{product.price}</p>
                     <button className="btn btn-danger">주문하기</button>
                     <button className="btn btn-danger" onClick={() => {
-                        // 이전 페이지로 이동 시키기
+                        // 1. 이전 페이지로 이동 시키기
                         history.goBack();
-                        // 특정 경로로 이동 시키기 : history.push('/');
+                        // 2. 특정 경로로 이동 시키기 : history.push('/');
                      }}>뒤로가기</button>
                 </div>
             </div>
